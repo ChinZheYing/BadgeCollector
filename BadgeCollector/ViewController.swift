96fb280d97ed68f7b6a24ce08098130a105ed153
Chin Zhe Ying <chinzheying@gmail.com>
2017-09-27 22:57:32 +0800

    Initial Commit

---
+++ BadgeCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BadgeCollector
+//
+//  Created by Chin Zhe Ying on Sep/27/17.
+//  Copyright © 2017 Chin Zhe Ying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
